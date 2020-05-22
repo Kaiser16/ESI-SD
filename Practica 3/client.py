ac7f1d8ea9bd422fef324665bf561b17fedbfa80
Kaiser16 <jesusmarquezdelgado@gmail.com>
2020-05-22 17:28:16 +0200

    ALL

---
+++ Practica 3/client.py
@@ -0,0 +1,150 @@
+import json
+import requests
+
+op = -1
+
+def imprimirHabitacion(habitacion):
+    id = habitacion['id']
+    plazas = habitacion['plazas']
+    equipamiento = habitacion['equipamiento']
+    n = len(equipamiento)
+    cadequip = equipamiento[0]
+    if(n > 1):
+        i = 1
+        while(i < n):
+            if(i < n-1):
+                cadequip += ", "+equipamiento[i]
+            else:
+                cadequip += " y "+equipamiento[i]
+            i += 1
+    ocupada = habitacion['ocupada']
+
+    print("Habitación "+str(id))
+    print("\t- "+plazas+" plazas")
+    print("\t- Equipamiento: "+cadequip)
+    if(ocupada):
+        print("\t- Ocupada")
+    else:
+        print("\t- Libre")
+
+def altaHabitacion():
+    plazas = input("Introduce el número de plazas: ")
+    equipamiento = input("Introduce el equipamiento separado por (,): ")
+    ocupada = input("Ocupada(s/n): ")
+    if(ocupada == "s"):
+        bOcupada = True
+    else:
+        bOcupada = False
+    res = requests.post("http://localhost:8080/altaHabitacion",json={"plazas": plazas, "equipamiento": equipamiento.split(","), "ocupada": bOcupada})
+    if(res.text == ""):
+        print("Los datos no se introdujeron correctamente")
+    else:
+        print("Se ha creado la siguiente habitación: ")
+        imprimirHabitacion(json.loads(res.text))
+
+def modificarHabitacion():
+    id = input("Identificador de la habitación: ")
+    op = -1
+    res = requests.get("http://localhost:8080/buscarHabitacion/"+id)
+    if(res.text == ""):
+        print("La habitación buscada no existe")
+    else:
+        imprimirHabitacion(res.json())
+        while(op != "0"):
+            print("\n|MODIFICAR HABITACIÓN "+id+"|")
+            print("1.Modificar plazas")
+            print("2.Modificar equipamiento")
+            print("3.Modificar estado de ocupación")
+            print("0.Salir") 
+            op = input("\nOpción: ")
+            if(op == "1"):
+                plazas = input("Introduce el número de plazas: ")
+                res = requests.put("http://localhost:8080/modificarHabitacion/"+id,json={"plazas": plazas})
+            elif(op == "2"):
+                equipamiento = input("Introduce el equipamiento separado por (,): ")
+                res = requests.put("http://localhost:8080/modificarHabitacion/"+id,json={"equipamiento": equipamiento.split(",")})
+            elif(op == "3"):
+                ocupada = input("Ocupada(s/n): ")
+                if(ocupada == "s"):
+                    bOcupada = True
+                else:
+                    bOcupada = False
+                res = requests.put("http://localhost:8080/modificarHabitacion/"+id,json={"ocupada": bOcupada})
+            if(op == "1" or op == "2" or op == "3"):
+                if(res.text == ""):
+                    print("La modificación introducida es incorrecta")
+                else:
+                    print("Habitación modificada:")
+                    imprimirHabitacion(json.loads(res.text))
+            elif op is not '0':
+                print('Operación no contemplada.\n')
+
+def listaHabitaciones():
+    res = requests.get("http://localhost:8080/listaHabitaciones")
+    if(res.text == "{}"):
+        print("No existen habitaciones")
+    else:
+        print("\n|LISTA DE HABITACIONES|")
+        for valor in res.json():
+            imprimirHabitacion(res.json()[valor])
+
+def buscarHabitacion():
+    id = input("Identificador de la habitacion: ")
+    res = requests.get("http://localhost:8080/buscarHabitacion/"+id)
+    if(res.text == ""):
+        print("La habitación buscada no existe")
+    else:
+        print("|HABITACIÓN "+id+"|")
+        imprimirHabitacion(res.json())
+
+def eliminarHabitacion():
+    id = input("Identificador de la habitacion: ")
+    res = requests.delete("http://localhost:8080/eliminarHabitacion/"+id)
+    if(res.text == ""):
+        print("La habitación a eliminar no existe")
+    else:
+        print("La habitacion "+res.text+" ha sido eliminada")
+
+def habitacionesDesocupadas():
+    res = requests.get('http://localhost:8080/habitaciones/Desocupadas')
+    if(res.text == "{}"):
+        print("No hay habitaciones vacías.")
+    else:
+        print("|LISTA DE HABITACIONES DESOCUPADAS|")
+        for valor in res.json():
+            imprimirHabitacion(valor)
+
+def habitacionesOcupadas():
+    res = requests.get('http://localhost:8080/habitaciones/Ocupadas')
+    if(res.text == "{}"):
+        print("No hay habitaciones ocupadas.")
+    else:
+        print("|LISTA DE HABITACIONES OCUPADAS|")
+        for valor in res.json():
+            imprimirHabitacion(valor)
+
+while(op != "0"):
+    print("\n|API HOTEL|")
+    print("1. Dar de alta habitación")
+    print("2. Modificar habitación")
+    print("3. Lista de habitaciones")
+    print("4. Buscar habitación")
+    print('5. Habitaciones desocupadas')
+    print('6. Habitaciones ocupadas')
+    print("7. Eliminar habitacion")
+    print("0. Salir")
+    op = input("\nOpción: ")
+    if(op == "1"):
+        altaHabitacion()
+    elif(op == "2"):
+        modificarHabitacion()
+    elif(op == "3"):
+        listaHabitaciones()
+    elif(op == "4"):
+        buscarHabitacion()
+    elif(op == '5'):
+        habitacionesDesocupadas()
+    elif(op == '6'):
+        habitacionesOcupadas()
+    elif(op == "7"):
+        eliminarHabitacion()
---
+++ Practica 3/server.py
@@ -0,0 +1,119 @@
+import json
+from bottle import run, request, response, get, post, put, delete
+
+class Room:
+    def __init__(self, id, plazas, equipamiento, ocupada):
+        self.id = id
+        self.plazas = plazas
+        self.equipamiento = equipamiento
+        self.ocupada = ocupada
+
+def guardar(data,filename):
+    f = open(filename,'w')
+    json.dump(data,f,indent=4)
+    f.close()
+
+def cargar(filename):
+    try:
+        f = open(filename)
+        datos = json.load(f)
+        f.close()
+    except:
+        datos = {}
+        datos['habitaciones'] = {}
+        datos['maxId'] = 0
+    return datos
+
+@post('/altaHabitacion')
+def alta_habitacion():
+
+    bd = cargar('bd.json')
+
+    habitaciones = bd['habitaciones']
+    
+    data = request.json
+
+    plazas = data.get('plazas')
+    if not plazas.isnumeric():
+        return ""
+    if int(plazas) <= 0:
+        return ""
+    equipamiento = data.get('equipamiento')
+    ocupada = data.get('ocupada')
+    
+    habitacion = Room(bd['maxId'],plazas,equipamiento,ocupada)
+
+    habitaciones[bd['maxId']] = habitacion.__dict__
+    bd['maxId'] += 1
+
+    response.headers['Content-Type'] = 'application/json'
+
+    guardar(bd,'bd.json')
+    return json.dumps(habitacion.__dict__)
+
+@put('/modificarHabitacion/<identificador>')
+def modificar_habitacion(identificador):
+    bd = cargar('bd.json')
+
+    habitacion = bd['habitaciones'][identificador]
+
+    data = request.json
+    plazas = data.get('plazas') 
+    equipamiento = data.get('equipamiento')
+    ocupada = data.get('ocupada')
+
+    if(plazas != None):
+        if(not plazas.isnumeric()):
+            return ""
+        else:
+            habitacion['plazas'] = plazas
+    if(equipamiento != None):
+        habitacion['equipamiento'] = equipamiento
+    if(ocupada != None):
+        habitacion['ocupada'] = ocupada
+
+    guardar(bd,'bd.json')
+
+    return habitacion
+
+@get('/buscarHabitacion/<identificador>')
+def buscar_habitacion(identificador):
+    try:
+        bd = cargar('bd.json')
+        return bd['habitaciones'][identificador]
+    except:
+        return ""
+
+@get('/listaHabitaciones')
+def lista_habitaciones():
+    bd = cargar('bd.json')
+    return bd['habitaciones']
+
+@delete('/eliminarHabitacion/<identificador>')
+def buscar_habitacion(identificador):
+    try:
+        bd = cargar('bd.json')
+        del bd['habitaciones'][identificador]
+        guardar(bd,'bd.json')
+        return identificador
+    except:
+        return ""
+
+@get('/habitaciones/<ocupada>')
+def habitaciones_ocupadas(ocupada):
+    bd = cargar('bd.json')
+    res = list()
+    if ocupada == 'Ocupadas':
+        ocupada = True
+    elif ocupada == 'Desocupadas':
+        ocupada = False
+    else:
+        return ""
+    for indice in bd['habitaciones']:
+        if bd['habitaciones'][indice]['ocupada'] is ocupada:
+            res.append(bd['habitaciones'][indice])
+    return '{}' if len(res) is 0 else json.dumps(res, indent=4)
+
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080, debug=True)
